caae24170a73002ae0a5a44239ecef1ad72b7206
aczarnek <czarnekanna@gmail.com>
2017-11-15 23:44:32 +0100

    Options in the main template are created from wrong table, but I did that only for tests, because Localization table doesn't contain any records yet. In script.py home function has also created cursor2 for tests. The rest of code shoul be the same as code on fixes branch created by you :)

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
 
-database=(
-    DATABASE='',
-    SECRET_KEY='',
-    USERNAME='',
-    PASSWORD=''
+database=dict(
+    DATABASE=os.path.join(app.root_path, 'app.db'),
+    SECRET_KEY='XXXXX',
+    USERNAME='XXXXX',
+    PASSWORD='XXXXX'
 )
--- app/script.py
+++ app/script.py
@@ -1,22 +1,12 @@
-from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash
-import sqlite3
-import os
-from config import database
-from database import *
-
-app=Flask(__name__)
-app.config.from_object(__name__)
-
-app.config.update(dict(database))
-
-app.config.from_envvar('APP_SETTINGS', silent=True)
+from flask import render_template
+from .database import *
 
 
 @app.route('/')
 def home():
     db = get_db()
-    cur = db.execute('select Name, Type from Event limit 5 order by Id desc')
-    cur2=db.execute('select distinct City from Localization order by City')
+    cur = db.execute('select Name, Type from Event order by Name desc')
+    cur2=db.execute('select distinct Description from Event order by Description')
     events = cur.fetchall()
     cities=cur2.fetchall()
     return render_template('main.html', events=events, cities=cities)
